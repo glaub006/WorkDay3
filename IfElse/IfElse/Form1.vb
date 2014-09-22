cfc16da47314a9329b00ddfe7dab03d7b1e8c5a8
idsc3101 <idsc3101@gmail.com>
2014-09-22 17:14:12 -0500

    Test

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -5,14 +5,23 @@ Public Class Form1
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
         Dim num1, num2, largerNum As Double
 
-        ' Modify the program such that it does not crash 
-        ' if the user types incorrect input
 
         Dim st1 As String = txtFirstNum.Text
         Dim st2 As String = txtSecondNum.Text
 
+        If Not (IsNumeric(st1) And IsNumeric(st2)) Then
+            MessageBox.Show("PLease enter in a number")
+            Return
+        End If
+
+
         num1 = CDbl(st1)
         num2 = CDbl(st2)
+        If Not (num1 >= 0 And num2 >= 0) Then
+            MessageBox.Show("error")
+            Return
+
+        End If
 
         Dim result As String
 
@@ -25,6 +34,8 @@ Public Class Form1
         End If
 
         txtResult.Text = result
+
+
     End Sub
 
 End Class
